f3b384ea9d4261396595d0d1e5fb698230d2c21a
Anthony_Logunov <skykeeper925@gmail.com>
2024-10-29 00:02:13 +0300

    Code formating for tags tab

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -1,86 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {Tag} from "../../models";
-import {TagsServiceService} from "../../service/tags-service.service";
-import {Router} from "@angular/router";
-import {LoadingComponent, SectionWrapperComponent} from "../../shared";
-import {TagsCardComponent} from "./tags-card/tags-card.component";
-import {CommonModule} from "@angular/common";
-import {BaseDataComponent} from "../based/based-data.component";
-import {LoadingService} from "../../service/loader.service";
-import {RequestRemindService} from "../../service/reminds-service.service";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { TagsServiceService } from '../../service/tags-service.service';
+import { LoadingComponent, SectionWrapperComponent } from '../../shared';
+import { TagsCardComponent } from './tags-card/tags-card.component';
+import { Tag } from '../../models';
+import { BaseDataComponent } from '../based/based-data.component';
+import { LoadingService } from '../../service/loader.service';
+import { RequestRemindService } from '../../service/reminds-service.service';
 
 @Component({
   selector: 'app-tags',
   standalone: true,
-  imports: [
-    SectionWrapperComponent,
-    TagsCardComponent,
-    CommonModule,
-    LoadingComponent
-  ],
+  imports: [SectionWrapperComponent, TagsCardComponent, CommonModule, LoadingComponent],
   templateUrl: './tags.component.html',
-  styleUrl: './tags.component.css'
+  styleUrl: './tags.component.scss',
 })
-export class TagsComponent extends BaseDataComponent<Tag> implements OnInit{
+export class TagsComponent extends BaseDataComponent<Tag> implements OnInit {
   constructor(
     protected tagService: TagsServiceService,
     protected override router: Router,
     protected remindService: RequestRemindService,
-    protected override loadingService: LoadingService) {
-    super(tagService, router,loadingService);
-
-  }
-
-  addTag = () => {
-    this.addItem('tags/add')
-  }
-
-  tagDetails = (tag: Tag) => {
-    const tagId = tag.id;
-    this.router.navigate(['tags', tagId]);
-  }
-
-  deleteTag = (tag: Tag): void => {
-    this.remindService.deleteTagFromReminds(tag.id)
-    this.deleteItem(tag.id)
-
-  }
-}
-
-
-
-
-
-/*
-  constructor(
-    protected tagService: TagsServiceService,
-    protected override router: Router,
-    protected override loadingService: LoadingService
+    protected override loadingService: LoadingService,
   ) {
-    super(tagService, router,loadingService);
+    super(tagService, router, loadingService);
   }
 
-  override ngOnInit() : void {
-    this.tagService.loadTags()
-  }
+  public addTag = (): void => {
+    this.addItem('tags/add');
+  };
 
-  loadCachedTags(): void {
-    this.tagService.getCachedTags().subscribe(tags => {
-      this.items = tags; // Используем items из BaseDataComponent для хранения тегов
-    });
-  }
-
-  addTag = ():void => {
-    this.addItem('tags/add')
-    this.tagService.addTagToCache()
-  }
-
-  tagDetails = (tag: Tag) => {
+  public tagDetails = (tag: Tag): void => {
     const tagId = tag.id;
     this.router.navigate(['tags', tagId]);
-  }
+  };
 
-  deleteTag = (tag: Tag): void => {
-    this.deleteItem(tag.id)
-  }
-*/
+  public deleteTag = (tag: Tag): void => {
+    this.remindService.deleteTagFromReminds(tag.id);
+    this.deleteItem(tag.id);
+  };
+}
