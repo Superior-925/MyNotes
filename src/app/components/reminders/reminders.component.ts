966392fcf2030cc52154ba8c33621f5306ab5312
Anthony_Logunov <skykeeper925@gmail.com>
2024-10-28 10:29:29 +0300

    Reformat reminders

--- src/app/components/reminders/reminders.component.ts
+++ src/app/components/reminders/reminders.component.ts
@@ -1,18 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {NotesDetailsComponent} from "../notes/notes-details/notes-details.component";
-import {NoteAddComponent} from "../notes/note-add/note-add.component";
-import {DevExtremeModule} from "devextreme-angular";
-import {Remind} from "../../models/remind";
-import {RequestRemindService} from "../../service/reminds-service.service";
-import {Router} from "@angular/router";
-import {SectionWrapperComponent} from "../../shared/section-wrapper/section-wrapper.component";
-import {RemindCardComponent} from "./remind-card/remind-card.component";
-import {TagsServiceService} from "../../service/tags-service.service";
-import {Tag} from "../../models";
-import {BaseDataComponent} from "../based/based-data.component";
-import {LoadingService} from "../../service/loader.service";
-import {LoadingComponent} from "../../shared";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { DevExtremeModule } from 'devextreme-angular';
+
+import { NotesDetailsComponent } from '../notes/notes-details/notes-details.component';
+import { NoteAddComponent } from '../notes/note-add/note-add.component';
+import { Remind, Tag } from '../../models';
+import { RequestRemindService } from '../../service/reminds-service.service';
+import { LoadingComponent, SectionWrapperComponent } from '../../shared';
+import { RemindCardComponent } from './remind-card/remind-card.component';
+import { TagsServiceService } from '../../service/tags-service.service';
+import { BaseDataComponent } from '../based/based-data.component';
+import { LoadingService } from '../../service/loader.service';
 
 @Component({
   selector: 'app-reminders',
@@ -27,56 +26,52 @@ import {LoadingComponent} from "../../shared";
     LoadingComponent,
   ],
   templateUrl: './reminders.component.html',
-  styleUrl: './reminders.component.css'
+  styleUrl: './reminders.component.scss',
 })
-export class RemindersComponent  extends BaseDataComponent<Remind> implements OnInit{
-
-  tags: Tag[] = []
-  currentRemind: Remind | null = null; // Для отображения в Popup
-
+export class RemindersComponent extends BaseDataComponent<Remind> implements OnInit {
+  public tags: Tag[] = [];
+  public currentRemind: Remind | null = null; // Для отображения в Popup
 
   constructor(
     protected override dataService: RequestRemindService,
     protected tagService: TagsServiceService,
     protected override router: Router,
     protected override loadingService: LoadingService,
-    private remindServ: RequestRemindService
+    private remindServ: RequestRemindService,
   ) {
-    super(dataService, router,loadingService);
-    this.tagService.getAll().subscribe((tags:Tag[]) => {
-      this.tags = tags
-    })
+    super(dataService, router, loadingService);
+    this.tagService.getAll().subscribe((tags: Tag[]) => {
+      this.tags = tags;
+    });
   }
 
-  override ngOnInit(): void {
+  public override ngOnInit(): void {
     super.ngOnInit();
     this.setupReminders();
   }
 
-  addRemind=():void => {
-    this.addItem('reminders/add')
-  }
+  public addRemind = (): void => {
+    this.addItem('reminders/add');
+  };
 
-  remindDetails = (remind: Remind) => {
+  public remindDetails = (remind: Remind): void => {
     const remindId = remind.id;
     this.router.navigate(['reminders', remindId]);
-  }
+  };
 
-  deleteRemind = (id:number) => {
-    this.deleteItem(id)
-  }
+  public deleteRemind = (id: number): void => {
+    this.deleteItem(id);
+  };
 
-  setupReminders(): void {
-    this.items.forEach(remind => {
+  private setupReminders(): void {
+    this.items.forEach((remind) => {
       this.remindServ.setReminder(remind);
     });
 
-    this.remindServ.remind$.subscribe(remind => {
+    this.remindServ.remind$.subscribe((remind) => {
       if (remind) {
         this.currentRemind = remind;
       }
     });
   }
-
 }
-
