648a220dedd6c3fe38f1a6192157a7fe2aa95452
Anthony_Logunov <skykeeper925@gmail.com>
2024-10-28 10:19:16 +0300

    Reformat remind-details component

--- src/app/components/reminders/remind-details/remind-details.component.ts
+++ src/app/components/reminders/remind-details/remind-details.component.ts
@@ -1,78 +1,56 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {Note} from "../../../models/note";
-import {Remind} from "../../../models/remind";
-import {ActivatedRoute, Router} from "@angular/router";
-import {RequestRemindService} from "../../../service/reminds-service.service";
-import {
-  DxButtonModule,
-  DxDateBoxModule,
-  DxPopupModule,
-  DxTagBoxModule,
-  DxTextAreaModule,
-  DxTextBoxModule
-} from "devextreme-angular";
-import {Tag} from "../../../models";
-import {TagsServiceService} from "../../../service/tags-service.service";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DxButtonModule, DxDateBoxModule, DxPopupModule, DxTagBoxModule, DxTextAreaModule, DxTextBoxModule } from 'devextreme-angular';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { RequestRemindService } from '../../../service/reminds-service.service';
+import { TagsServiceService } from '../../../service/tags-service.service';
+import { Remind, Tag } from '../../../models';
 
 @Component({
   selector: 'app-remind-details',
   standalone: true,
-  imports: [
-    DxPopupModule,
-    DxTextBoxModule,
-    DxTextAreaModule,
-    DxTagBoxModule,
-    DxDateBoxModule,
-    DxButtonModule
-  ],
+  imports: [DxPopupModule, DxTextBoxModule, DxTextAreaModule, DxTagBoxModule, DxDateBoxModule, DxButtonModule],
   templateUrl: './remind-details.component.html',
-  styleUrl: './remind-details.component.css'
+  styleUrl: './remind-details.component.scss',
 })
-export class RemindDetailsComponent {
+export class RemindDetailsComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
   @Input() remind!: Remind;
 
-  tags: Tag[] = []
-  isPopupVisible = false;
+  public tags: Tag[] = [];
+  public isPopupVisible = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private reqService: RequestRemindService,
     private router: Router,
-    private tagsService : TagsServiceService
-  ){}
+    private tagsService: TagsServiceService,
+  ) {}
 
-  ngOnInit() {
+  public ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
-      this.reqService.get(params['id']).subscribe((res:Remind) =>{
-        this.remind =res;
-        this.isPopupVisible = true
-      })
-    })
+      this.reqService.get(params['id']).subscribe((res: Remind) => {
+        this.remind = res;
+        this.isPopupVisible = true;
+      });
+    });
 
     this.tagsService.getAll().subscribe((tags) => {
-      this.tags = tags
-    })
+      this.tags = tags;
+    });
   }
 
-  closePopup(): void {
-    this.isPopupVisible = false;
-    this.close.emit();
-    this.router.navigate(['reminders'])
-  }
-
-
-  save(): void {
+  public save(): void {
     // Сначала получаем текущие выбранные ID тегов из remind
-    const selectedTagIds = this.remind.tags.map(tag => tag.id);
+    const selectedTagIds = this.remind.tags.map((tag) => tag.id);
 
     // Затем находим полные объекты тегов, соответствующие этим ID
-    const selectedTags = this.tags.filter(tag => selectedTagIds.includes(tag.id));
+    const selectedTags = this.tags.filter((tag) => selectedTagIds.includes(tag.id));
 
     // Обновляем remind с полными объектами тегов
     const updatedRemind: Remind = {
       ...this.remind,
-      tags: selectedTags
+      tags: selectedTags,
     };
 
     this.reqService.update(updatedRemind).subscribe(() => {
@@ -81,6 +59,9 @@ export class RemindDetailsComponent {
     });
   }
 
-
-
+  private closePopup(): void {
+    this.isPopupVisible = false;
+    this.close.emit();
+    this.router.navigate(['reminders']);
+  }
 }
