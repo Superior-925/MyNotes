1c741b99abcc17213b5cf489f689ddf8a80631be
Anthony_Logunov <skykeeper925@gmail.com>
2024-10-28 10:32:42 +0300

    Reformat tags

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,43 +1,47 @@
 import { Component } from '@angular/core';
-import {DxDrawerModule, DxListModule, DxToolbarModule} from "devextreme-angular";
-import {Router, RouterModule} from "@angular/router";
-import {List, SidebarServiceService as Service} from '../../service/sidebar-service.service'
+import { DxDrawerModule, DxListModule, DxToolbarModule } from 'devextreme-angular';
+import { Router, RouterModule } from '@angular/router';
+
+import { List, SidebarServiceService as Service } from '../../service/sidebar-service.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DxDrawerModule,DxListModule, RouterModule, DxToolbarModule],
+  imports: [DxDrawerModule, DxListModule, RouterModule, DxToolbarModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-  isDrawerOpen = true;
-  text: string;
-  navigation: List[];
+  public isDrawerOpen = true;
+  public text: string;
+  public navigation: List[];
 
-  constructor(private service: Service, private router: Router) {
+  constructor(
+    private service: Service,
+    private router: Router,
+  ) {
     this.text = service.getContent();
     this.navigation = service.getNavigationList();
   }
-  toolbarContent = [{
-    widget: 'dxButton',
-    location: 'before',
-    options: {
-      icon: 'menu',
-      stylingMode: 'text',
-      onClick: () => this.isDrawerOpen = !this.isDrawerOpen,
-    },
-  }];
 
-  onNavigationItemClicked(item: List): void {
-    //debugger
+  toolbarContent = [
+    {
+      widget: 'dxButton',
+      location: 'before',
+      options: {
+        icon: 'menu',
+        stylingMode: 'text',
+        onClick: () => (this.isDrawerOpen = !this.isDrawerOpen),
+      },
+    },
+  ];
+
+  public onNavigationItemClicked(item: List): void {
     const path = this.getPathByText(item.text);
     this.router.navigate([path]);
   }
 
-  getPathByText(text: string): string {
+  private getPathByText(text: string): string {
     return text.toLowerCase();
   }
-
-
 }
--- src/app/components/tags/tags-add/tags-add.component.ts
+++ src/app/components/tags/tags-add/tags-add.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {TagsServiceService} from "../../../service/tags-service.service";
-import {Remind, Tag} from "../../../models";
-import {catchError, throwError} from "rxjs";
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import {
   DxButtonModule,
-  DxDateBoxModule, DxFormModule,
+  DxDateBoxModule,
+  DxFormModule,
   DxPopupModule,
   DxTagBoxModule,
   DxTextAreaModule,
-  DxTextBoxModule
-} from "devextreme-angular";
-import {CommonModule} from "@angular/common";
+  DxTextBoxModule,
+} from 'devextreme-angular';
+
+import { TagsServiceService } from '../../../service/tags-service.service';
+import { Tag } from '../../../models';
 
 @Component({
   selector: 'app-tags-add',
@@ -30,56 +32,49 @@ import {CommonModule} from "@angular/common";
     DxFormModule,
   ],
   templateUrl: './tags-add.component.html',
-  styleUrl: './tags-add.component.css'
+  styleUrl: './tags-add.component.scss',
 })
 export class TagsAddComponent {
-
-  remindForm: FormGroup
+  remindForm: FormGroup;
   isPopupVisible = true;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private  tagService: TagsServiceService
+    private tagService: TagsServiceService,
   ) {
     this.remindForm = this.formBuilder.group({
       tagTitle: ['', Validators.required],
-    })
-
+    });
   }
 
-  get f(): any {
-    return this.remindForm.controls;
-  }
-
-  ngOnInit(): void {
-  }
-
-  save(): void {
+  public save(): void {
     if (this.remindForm.invalid) {
-      return
+      return;
     }
-
 
     const tag: Tag = {
       id: 0,
-      tagTitle: this.f.tagTitle.value,
+      tagTitle: this.formControls.tagTitle.value,
     };
 
-    this.tagService.add(tag as Tag)
+    this.tagService
+      .add(tag as Tag)
       .pipe(
-        catchError(error => {
+        catchError((error) => {
           console.error('Ошибка при добавлении напоминания:', error);
           return throwError(error);
-        })
+        }),
       )
       .subscribe(() => this.router.navigate(['tags']));
-
   }
 
-  closePopup(): void {
+  public closePopup(): void {
     this.isPopupVisible = false;
-    this.router.navigate(['tags'])
+    this.router.navigate(['tags']);
   }
 
+  private get formControls(): any {
+    return this.remindForm.controls;
+  }
 }
