3527eff49380052ba3ce7a54823e91d9d2da66ae
Anthony_Logunov <skykeeper925@gmail.com>
2024-10-28 23:55:58 +0300

    Used prettier for card item component

--- src/app/components/tags/tags-card/tags-card.component.ts
+++ src/app/components/tags/tags-card/tags-card.component.ts
@@ -1,29 +1,23 @@
-import {Component, Input} from '@angular/core';
-import {Tag} from "../../../models";
-import {CardItemComponent} from "../../../shared";
+import { Component, Input } from '@angular/core';
+import { Tag } from '../../../models';
+import { CardItemComponent } from '../../../shared';
 
 @Component({
   selector: 'app-tags-card',
   standalone: true,
-  imports: [
-    CardItemComponent
-  ],
+  imports: [CardItemComponent],
   templateUrl: './tags-card.component.html',
-  styleUrl: './tags-card.component.css'
+  styleUrl: './tags-card.component.css',
 })
 export class TagsCardComponent {
   @Input() tag!: Tag; // Добавляем список всех тегов
   @Input() deleteParentFunc!: (tag: Tag) => void;
   @Input() editParentFunc!: (tag: Tag) => void;
 
-  ngOnInit (): void {
-    console.log(this.tag)
-    console.log(this.tag)
-  }
-  onDelete =(): void => {
-    this.deleteParentFunc(this.tag)
-  }
-  onEdit =(): void=> {
-    this.editParentFunc(this.tag)
-  }
+  onDelete = (): void => {
+    this.deleteParentFunc(this.tag);
+  };
+  onEdit = (): void => {
+    this.editParentFunc(this.tag);
+  };
 }
--- src/app/shared/card-item/card-item.component.ts
+++ src/app/shared/card-item/card-item.component.ts
@@ -1,26 +1,24 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {DxButtonModule} from "devextreme-angular";
-import {Remind} from "../../models/remind";
-import {DisplayableItem} from "../../models/DisplayableItem";
+import { Component, Input } from '@angular/core';
+import { DxButtonModule } from 'devextreme-angular';
+import { DisplayableItem } from '../../models/DisplayableItem';
 
 @Component({
   selector: 'app-card-item',
   standalone: true,
-    imports: [
-        DxButtonModule
-    ],
+  imports: [DxButtonModule],
   templateUrl: './card-item.component.html',
-  styleUrl: './card-item.component.css'
+  styleUrl: './card-item.component.css',
 })
 export class CardItemComponent {
   @Input() item!: DisplayableItem;
-  @Input() delete !: () => void
-  @Input() edit !: () => void
+  @Input() delete!: () => void;
+  @Input() edit!: () => void;
 
-  onDelete = ():void => {
-    this.delete()
-  }
-  onEdit =():void => {
-    this.edit()
-  }
+  public onDelete = (): void => {
+    this.delete();
+  };
+
+  public onEdit = (): void => {
+    this.edit();
+  };
 }
